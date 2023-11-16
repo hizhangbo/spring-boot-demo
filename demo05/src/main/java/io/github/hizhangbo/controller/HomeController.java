451fc4fb8ad2705c9aa6c314df004238000c4ebc
larry.zhang <larry.zhang@yeahmobi.com>
2023-11-16 13:39:59 +0800

    test redisTemplate setIfAbsent

--- demo05/src/main/java/io/github/hizhangbo/controller/HomeController.java
+++ demo05/src/main/java/io/github/hizhangbo/controller/HomeController.java
@@ -1,14 +1,20 @@
 package io.github.hizhangbo.controller;
 
 import io.github.hizhangbo.GuavaCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class HomeController {
+
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
@@ -31,5 +37,16 @@ public class HomeController {
 
         guavaCacheManager.put(key, cckk);
         return cckk;
+    }
+
+    // 测试 setIfAbsent
+    @GetMapping("/absent")
+    public String redisIfAbsent() {
+
+        final Boolean absent1 = redisTemplate.opsForValue().setIfAbsent("absent", "123456789", 3, TimeUnit.SECONDS);
+        final Boolean absent2 = redisTemplate.opsForValue().setIfAbsent("absent", "123456789", 3, TimeUnit.SECONDS);
+
+        log.info("1: {}, 2: {}", absent1, absent2);
+        return "ok";
     }
 }
