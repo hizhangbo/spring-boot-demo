2b1d2dc2f8d41bbe6837055a08780025c8744d5e
larry.zhang <larry.zhang@yeahmobi.com>
2023-11-30 18:09:33 +0800

    add demo08

---
+++ demo08/src/main/java/io/github/hizhangbo/Demo08.java
@@ -0,0 +1,11 @@
+package io.github.hizhangbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo08 {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo08.class, args);
+    }
+}
