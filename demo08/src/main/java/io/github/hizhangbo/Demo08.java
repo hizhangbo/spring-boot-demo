b480dea1fcad0df4dd545b58e6f55d021595fee8
bob <hizhangbo@outlook.com>
2023-12-01 22:33:57 +0800

    add demo08 native compile

--- demo08/src/main/java/io/github/hizhangbo/Demo08.java
+++ demo08/src/main/java/io/github/hizhangbo/Demo08.java
@@ -3,6 +3,13 @@ package io.github.hizhangbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * GraalVm Native Compile
+ *
+ * mvn -Pnative -DskipTests=true clean package
+ * native:compile-no-fork
+ */
 @SpringBootApplication
 public class Demo08 {
     public static void main(String[] args) {
